52a1fd774125e80e8485159b5afb2db73f99f1ea
mmzyk <mmzyk@opscode.com>
2015-05-19 10:02:50 -0400

    Add eunit tests for contains_admin_group
    Adds some eunit tests for checking that the admin group is properly
    reported not removed.

--- src/oc_erchef/apps/oc_chef_authz/test/oc_chef_authz_acl_constraints_tests.erl
+++ src/oc_erchef/apps/oc_chef_authz/test/oc_chef_authz_acl_constraints_tests.erl
@@ -23,12 +23,32 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+%% Test generators that setup and run the tests
 contains_admin_group_test_() ->
   [
     { "Reports that admins group is found", contains_admin_group_true() },
     { "Reports that admins group is not found", contains_admin_group_false() }
   ].
 
+check_admin_group_removal_test_() ->
+  %% Use foreach so that the setup and teardown are run for each tests
+  %% so that any mocks/stubs are reset
+  { foreach, fun setup/0, fun teardown/1, [fun check_admin_group_removal_tests/1] }.
+
+
+%% Setup/teardown functions.
+%% Not used for all tests, only those that need mocking/stubbing
+setup() ->
+  %% Use meck to mock out the module, but set passthrough
+  %% so only those functions called with meck:expect are mocked
+  %% mocking/stubbing
+  meck:new(oc_chef_authz_acl_constraints, [passthrough]).
+
+teardown(_) ->
+  meck:unload(oc_chef_authz_acl_constraints).
+
+
+%% contains_admin_group tests
 contains_admin_group_true() ->
   [
     ?_assertEqual(true, oc_chef_authz_acl_constraints:contains_admin_group([<<"admins">>])),
@@ -40,3 +60,20 @@ contains_admin_group_false() ->
     ?_assertEqual(false, oc_chef_authz_acl_constraints:contains_admin_group([])),
     ?_assertEqual(false, oc_chef_authz_acl_constraints:contains_admin_group([<<"another_group">>]))
   ].
+
+
+%% check_admin_group_removal_tests
+check_admin_group_removal_tests(_) ->
+  [
+    { "Check admin group removal returns admins group is not removed", check_admin_group_removal_not_removed() }
+  ].
+
+check_admin_group_removal_not_removed() ->
+  meck:expect(oc_chef_authz_acl_constraints, contains_admin_group, fun(_Group) -> false end),
+  [
+    %% CurrentGroup doesn't contain the admins group
+    ?_assertEqual(not_removed, oc_chef_authz_acl_constraints:check_admin_group_removal([], [])),
+    %% CurrentGroup contains admins group, NewGroup also contains admin group
+    ?_assertEqual(not_removed, oc_chef_authz_acl_constraints:check_admin_group_removal([<<"admins">>], [<<"admins">>]))
+  ].
+
